18f0232e1b9ccf3f3287d51ae6d536a8843d988f
Perflyst <Perflyst@users.noreply.github.com>
2017-06-07 21:21:02 +0200

    Update zahlen.py

--- python/zahlen.py
+++ python/zahlen.py
@@ -4,11 +4,12 @@ try:
     start = int(sys.argv[1])
     end = int(sys.argv[2])
     out = sys.argv[3]
-except:
+
+except Exception as error:
     print("error")
     sys.exit(-1)
-    
-out_file = open(out, "a")
-for i in range(start, end):
-    out_file.write(str(i)+'\n')
+
+out_file = open(out, "a")  # a versucht Datei zu öffnen, wenn nicht da, erstellen
+for i in range(start, end):  # range erstellt Zahlen zwischen start & stop
+    out_file.write(str(i) + '\n')  # i wird jede Runde einen Wert höher
 out_file.close()
